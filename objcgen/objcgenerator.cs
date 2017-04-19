6db40a88f2fc4ef339f6cd0759f397a3a9889aea
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-04-19 13:02:54 +0200

    [objc] Add an initialization method for the generated registrar code.

--- objcgen/objcgenerator.cs
+++ objcgen/objcgenerator.cs
@@ -58,6 +58,13 @@ namespace ObjC {
 			implementation.WriteLine ("}");
 			implementation.WriteLine ();
 
+			// The generated registrar code calls this method on every entry point.
+			implementation.WriteLine ("void xamarin_embeddinator_initialize ()");
+			implementation.WriteLine ("{");
+			implementation.WriteLine ("\t__initialize_mono ();");
+			implementation.WriteLine ("}");
+			implementation.WriteLine ();
+
 			base.Generate (assemblies);
 		}
 
